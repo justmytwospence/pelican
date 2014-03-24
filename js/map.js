017cab8a1cea8cdf0ac145a416eab4027fae810b
justmytwospence <spencer.g.boucher@gmail.com>
2014-03-23 20:53:46 -0800

    Update documentation

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 <script src="http://maps.google.com/maps/api/js?v=3.2&sensor=false"></script>
 <script src="http://libs.cartocdn.com/cartodb.js/v3/cartodb.js"></script>
 <script>
-function init() {
-	cartodb.createVis('map', 'http://justmytwospence.cartodb.com/api/v2/viz/e8fd87d0-78b3-11e3-a9e9-e7941b6e2df0/viz.json');
-}
+	function init() {
+		cartodb.createVis('map', 'http://justmytwospence.cartodb.com/api/v2/viz/e8fd87d0-78b3-11e3-a9e9-e7941b6e2df0/viz.json');
+	}
 console.log('Successfully sourced')
 </script>
