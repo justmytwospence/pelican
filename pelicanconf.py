6b74bc48f834d9ddb07966e6e6a06bbf2fc98bab
Spencer Boucher <spencer.g.boucher@gmail.com>
2014-04-01 14:18:36 -0700

    Making favicon work

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,7 +15,7 @@ BOOTSTRAP_NAVBAR_INVERSE = True
 #BOOTSTRAP_THEME = 'darkly'
 TYPOGRIFY = True
 PYGMENTS_STYLE = 'github'
-FAVICON = 'favicon.png'
+FAVICON = '/favicon.ico'
 
 # Display Switches
 DISPLAY_ARCHIVES = False
@@ -46,7 +46,7 @@ USE_FOLDER_AS_CATEGORY = True
 STATIC_PATHS = ['images', 'pdfs', 'extra']
 EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'},
-    'extra/favicon.png': {'path': 'favicon.png'}
+    'extra/favicon.ico': {'path': 'favicon.ico'}
 }
 PLUGIN_PATH = 'pelican-plugins'
 PLUGINS = ['liquid_tags.notebook',
