a79b5a0ccaa9afc47aea6593e961d2a54011b857
Spencer Boucher <spencer.g.boucher@gmail.com>
2014-04-12 14:03:23 -0800

    Update documentation

--- theme/js/dynamic.js
+++ theme/js/dynamic.js
@@ -12,25 +12,4 @@ $(document).ready(function () {
     	placement: "left"
     })
 
-    $(function(){
-        $('.navbar').data('size','big');
-    });
-    
-    $(window).scroll(function(){
-        var $nav = $('#header_nav');
-        if ($('body').scrollTop() > 0) {
-            if ($nav.data('size') == 'big') {
-                $nav.data('size','small').stop().animate({
-                    height:'40px'
-                }, 600);
-            }
-        } else {
-            if ($nav.data('size') == 'small') {
-                $nav.data('size','big').stop().animate({
-                    height:'100px'
-                }, 600);
-            }  
-        }
-    });
-
 })
