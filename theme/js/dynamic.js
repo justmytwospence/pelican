fcd418822b29c6a43bc59c5f2984f0f57ef38397
Spencer Boucher <spencer.g.boucher@gmail.com>
2014-04-13 12:03:13 -0800

    Update documentation

--- theme/js/dynamic.js
+++ theme/js/dynamic.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
     $('.Design').tooltip({
     	title: "The commonality between science and art is in trying to see profoundly - to develop strategies of seeing and showing. -Edward Tufte",
-    	placement: "left",
+    	placement: "top auto",
     })
     $('.Misc').tooltip({
     	title: "Miscellania that doesn't fit anywhere else.",
-    	placement: "left"
+    	placement: "top auto"
     })
     $('.Python').tooltip({
     	title: "Beautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.",
-    	placement: "left"
+    	placement: "top auto"
     })
 
 })
